494265028e7093f694e95e682593da63eb112f16
Crash-Zeus <jonas.bertin49@gmail.com>
2020-01-22 20:07:55 +0100

    [UPDATE] client with persistance

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
 import socket
 
+host = 'localhost'
+port = 12800
+
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(('localhost', 12800))
-message = connection.recv(1024)
-print(message)
+connection.connect((host, port))
+print("Connection established with the server on port {}".format(port))
+
+sendingMesg = ""
+while sendingMesg != b"fin":
+    sendingMesg = input(">>> ")
+    # Sometimes goes crash with special char
+    sendingMesg = sendingMesg.encode()
+    connection.send(sendingMesg)
+    receivingMsg = connection.recv(1024)
+    print(receivingMsg.decode())
+
+print("Closing connection")
+connection.close()
