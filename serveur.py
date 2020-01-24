eb26293adf81293c0b1ce3e9294b60eae3153eaa
maxicoffee <j.bertin@maxicoffee.com>
2020-01-24 13:54:21 +0100

    [FIX] exception as probleme [UPDATE]  set port var [UPDATE] trying to patch input in server for closing him

--- serveur.py
+++ serveur.py
@@ -3,7 +3,7 @@ import socket
 import time
 import select
 
-serverUp = True
+
 class ClientThread(threading.Thread):
 
     def __init__(self, host, port, clientsocket):
@@ -14,36 +14,52 @@ class ClientThread(threading.Thread):
         self.clientsocket = clientsocket
         print("[+] New thread for %s %s" % (self.host, self.port, ))
 
-    def run(self): 
+    def run(self):
         print("Connection of %s %s" % (self.host, self.port, ))
         while serverUp == True:
             try:
                 request = self.clientsocket.recv(2048)
-            except Exception as e:
+            except Exception:
                 pass
             if request.decode() != "end":
-                    print(request.decode())
-                    try:
-                        self.clientsocket.send("Server ok -".encode())
-                    except Exception as e:
-                        print("[-] Thread killed for %s %s" % (self.host, self.port))
-                        self.clientsocket.close()
-                        self.killed = True
-                        break
+                print(request.decode())
+                try:
+                    self.clientsocket.send("Server ok -".encode())
+                except Exception:
+                    print("[-] Thread killed for %s %s" %
+                          (self.host, self.port))
+                    self.clientsocket.close()
+                    self.killed = True
+                    break
 
 
-port = 1111
+serverUp = True
+
+port = 1112
 
 mainConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mainConnection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-mainConnection.bind(("",1111))
+mainConnection.bind(("", port))
 
 while serverUp == True:
     try:
         mainConnection.listen(10)
-        print( "listening on %s" % (port))
+        print("listening on %s" % (port))
         (clientsocket, (host, port)) = mainConnection.accept()
         newthread = ClientThread(host, port, clientsocket)
         newthread.start()
+        if newthread is None:
+            row = input(">>> ")
+            if row == "stop":
+                try:
+                    newthread._stop()
+                except Exception:
+                    pass
+                serverUp = False
     except KeyboardInterrupt:
-        newthread._stop()
+        try:
+            newthread._stop()
+        except Exception:
+            # Catach & pass exception with none thread open
+            pass
+        serverUp = False
